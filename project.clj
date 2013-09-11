af2937332208942130dbb4645bff37cd1113fa67
Erick Neves <neves.erick@gmail.com>
2013-09-11 17:40:08 -0300

    changes in 'project.clj' for building a proper jar to deploy to a storm cluster

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject storm-ex01 "0.1.0-SNAPSHOT"
+(defproject storm_ex02 "0.0.1-SNAPSHOT"
   :repositories {"spy" "http://files.couchbase.com/maven2/"}
+  :profiles {:provided {:dependencies [[storm "0.9.0-wip21"]]}}
   :dependencies [[commons-collections/commons-collections "3.2.1"]
-    [storm "0.8.2"]
     [org.clojure/clojure "1.4.0"]
     [org.clojure/clojure-contrib "1.2.0"]
     [spy/spymemcached "2.7.1"]]
-  :main storm-ex02.core)
+  :main storm_ex02.core
+  :aot :all
+  :manifest {"Class-Path" "lib/clojure-contrib-1.2.0.jar"})
--- src/storm_ex02/core.clj
+++ src/storm_ex02/core.clj
@@ -1,40 +1,22 @@
-(ns storm-ex02.core
-  (:import [backtype.storm LocalCluster]
-           [net.spy.memcached MemcachedClient]
+(ns storm_ex02.core
+  (:import [backtype.storm StormSubmitter LocalCluster]
            [java.net InetSocketAddress])
   (:use [backtype.storm clojure config])
   (:use [clojure.contrib.def :only [defn-memo]])
-  (:gen-class))
+  
+  (:gen-class :name storm_ex02.core))
 
-(defn- create-client
-    [& {:keys [host port]
-              :or {host "localhost" port 22133}}]
-       (MemcachedClient. (list (InetSocketAddress. host port))))
-
-(defn-memo default-client
-               [& {:keys [host port]
-                   :or {host "localhost" port 22133}}]
-                    (create-client host port))
-
-(defn get-item
-    [queue-name]
-    (.get (default-client) queue-name))
 
 (defspout initial-number ["number"]
   [conf context collector]
   (spout
     (nextTuple []
       (Thread/sleep 100)
-        (let [item (get-item "teste")]         
-          (if item
-            (emit-spout! collector [(read-string item)]))))))
+      ;(let [item (get-item "teste")]         
+      ;(if item
+      (emit-spout! collector [123]))))
 
 (defbolt count1 ["add-number"]
-  [tuple collector]
-        (let [number (.getLong tuple 0)]
-          (emit-bolt! collector [(+ 1 number)])))
-
-(defbolt count2 ["add-number2"]
   [tuple collector]
     (let [number (.getLong tuple 0)]
       (emit-bolt! collector [(+ 1 number)])))
@@ -43,13 +25,22 @@
   (topology
     {"number-spout" (spout-spec initial-number)}
     {"adder-bolt-1" (bolt-spec {"number-spout" :shuffle} count1 :p 1)}))
-    ;"adder-bolt-2" (bolt-spec {"adder-bolt-1" :shuffle} count2 :p 1)}))
 
 (defn run-local! []
-  (let [cluster (LocalCluster.)]
-    (.submitTopology cluster "reduce-counter" {TOPOLOGY-DEBUG true} (mk-topology))
-    (Thread/sleep 50000)
-    (.shutdown cluster)))
+	(let [cluster (LocalCluster.)]
+		(.submitTopology cluster "reduce-counter" {TOPOLOGY-DEBUG true} (mk-topology))
+		(Thread/sleep 50000)
+		(.shutdown cluster)))
 
-(defn -main []
+(defn submit-topology! [name]
+  (StormSubmitter/submitTopology
+   name
+   {TOPOLOGY-DEBUG true
+    TOPOLOGY-WORKERS 1}
+   (mk-topology)))
+
+(defn -main
+  ([]
    (run-local!))
+  ([name]
+   (submit-topology! name)))
